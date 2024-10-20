031d9c07f9a93b65fa615ac2892150c12d23ec98
Mircea Badragan <mircea.badragan@monsterlynx.com>
2024-10-20 13:36:56 +0300

    chore: improve gitsigns icons and bring back lazy git

--- config/nvim/lua/custom/plugins/git.lua
+++ config/nvim/lua/custom/plugins/git.lua
@@ -3,6 +3,27 @@ return {
   -- {
   --   'tpope/vim-fugitive',
   -- },
+  --
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = true,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
   {
     'NeogitOrg/neogit',
     dependencies = {
@@ -24,11 +45,20 @@ return {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
+        add = { text = '┃' },
+        change = { text = '┃' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
       },
     },
   },
