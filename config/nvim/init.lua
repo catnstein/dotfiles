8042ddb9ea84e7cd9ec0e98fe08688e6ae6c7459
Mircea Badragan <mircea.badragan@monsterlynx.com>
2024-10-20 13:39:06 +0300

    chore: up to date lazy lock

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -198,7 +198,9 @@ require('lazy').setup {
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
+  spec = {
+    { import = 'custom.plugins' },
+  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
