edf40e5b1550df72961b1df80b3c4465662a70ae
Mircea Badragan <mircea.badragan@monsterlynx.com>
2024-11-10 09:44:27 +0200

    chore: remove neogit

--- config/nvim/lua/custom/plugins/git.lua
+++ config/nvim/lua/custom/plugins/git.lua
@@ -24,22 +24,6 @@ return {
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- required
-      'sindrets/diffview.nvim', -- optional - Diff integration
-
-      -- Only one of these is needed, not both.
-      'nvim-telescope/telescope.nvim', -- optional
-      -- 'ibhagwan/fzf-lua', -- optional
-    },
-    config = true,
-  },
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following lua:
-  --    require('gitsigns').setup({ ... })
-  --
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
