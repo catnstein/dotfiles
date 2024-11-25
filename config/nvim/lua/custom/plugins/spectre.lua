5e082367068066d97de2bd9ff89fa94674b386cc
Mircea Badragan <mircea.badragan@monsterlynx.com>
2024-11-25 19:43:57 +0200

    feat: add nvim spectre

---
+++ config/nvim/lua/custom/plugins/spectre.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-pack/nvim-spectre',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('spectre').setup()
+  end,
+}
