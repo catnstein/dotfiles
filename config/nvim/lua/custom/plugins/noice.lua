23f54cbf643dc0cc8039d01d27cf8f8a9ca34638
Mircea Badragan <mircea.badragan@monsterlynx.com>
2024-11-25 19:45:59 +0200

    chore: cleanup some comments

--- config/nvim/lua/custom/plugins/noice.lua
+++ config/nvim/lua/custom/plugins/noice.lua
@@ -2,8 +2,6 @@ return {
   'folke/noice.nvim',
   event = 'VeryLazy',
   opts = {
-    -- add any options here
-    -- fix for macro command not showing
     routes = {
       {
         view = 'cmdline',
@@ -20,11 +18,7 @@ return {
     },
   },
   dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     'MunifTanjim/nui.nvim',
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     'rcarriga/nvim-notify',
   },
 }
