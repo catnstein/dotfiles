de5dec0d8ab6733b85db43f22b8457fe6bc8a355
Mircea Badragan <mircea.badragan@monsterlynx.com>
2025-05-21 17:48:34 +0300

    fix: prettier error not stopping at first

--- config/nvim/lua/custom/plugins/conform.lua
+++ config/nvim/lua/custom/plugins/conform.lua
@@ -27,7 +27,7 @@ return {
       --
       -- You can use a sub-list to tell conform to run *until* a formatter
       -- is found.
-      javascript = { 'prettierd', 'prettier' },
+      javascript = { 'prettierd', 'prettier', stop_after_first = true },
       typescript = { 'prettierd', 'prettier' },
       javascriptreact = { 'prettierd', 'prettier' },
       typescriptreact = { 'prettierd', 'prettier' }, -- Add this for TSX support
