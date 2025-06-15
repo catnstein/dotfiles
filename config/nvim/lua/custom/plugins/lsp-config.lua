31a26a7d4b95547d663ec6f3be12061a4bc81d71
Mircea Badragan <mircea.badragan@monsterlynx.com>
2025-06-15 21:03:12 +0300

    chore: move some lsps into native neovim lsp functionality

--- config/nvim/lua/custom/plugins/lsp-config.lua
+++ config/nvim/lua/custom/plugins/lsp-config.lua
@@ -137,7 +137,6 @@ return { -- LSP Configuration & Plugins
     local servers = {
       -- clangd = {},
       -- golangci_lint_ls = {},
-      gopls = {},
       -- denols = {
       --   -- on_attach = on_attach,
       --   root_dir = nvim_lsp.util.root_pattern('deno.json', 'deno.jsonc'),
@@ -174,36 +173,6 @@ return { -- LSP Configuration & Plugins
       -- },
       angularls = {
         capabilities = {},
-      },
-      html = {},
-      pylsp = {},
-
-      lua_ls = {
-        -- cmd = {...},
-        -- filetypes { ...},
-        -- capabilities = {},
-        settings = {
-          Lua = {
-            hint = { enable = true },
-            runtime = { version = 'LuaJIT' },
-            workspace = {
-              checkThirdParty = false,
-              -- Tells lua_ls where to find all the Lua files that you have loaded
-              -- for your neovim configuration.
-              library = {
-                '${3rd}/luv/library',
-                unpack(vim.api.nvim_get_runtime_file('', true)),
-              },
-              -- If lua_ls is really slow on your computer, you can try this instead:
-              -- library = { vim.env.VIMRUNTIME },
-            },
-            completion = {
-              callSnippet = 'Replace',
-            },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
-          },
-        },
       },
     }
 
