cd7d8638277b0a68263f1eba34f53bc8cdfcca05
Mircea Badragan <mircea.badragan@monsterlynx.com>
2025-11-30 23:27:11 +0200

    feat: improve lualine

--- config/nvim/lua/custom/plugins/lualine.lua
+++ config/nvim/lua/custom/plugins/lualine.lua
@@ -40,12 +40,26 @@ return {
 
     require('lualine').setup {
       options = {
-        -- theme = 'catppuccin',
+        theme = 'catppuccin-mocha',
         globalstatus = true,
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '█', right = '█' },
+        component_separators = '',
+        section_separators = { left = '', right = '' },
       },
       sections = {
+        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+        lualine_y = {
+          { 'progress', separator = ' ', padding = { left = 1, right = 0 } },
+          { 'location', padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          {
+            function()
+              return ' ' .. os.date '%R'
+            end,
+            separator = { right = '' },
+          },
+        },
+
         lualine_b = {
           { 'branch', icon = '', fmt = truncate_branch_name },
           harpoon_component,
