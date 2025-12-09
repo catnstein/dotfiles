acd66dbc3828ce18ec2835d45b3d41f87f12a864
Mircea Badragan <mircea.badragan@monsterlynx.com>
2025-12-09 23:01:02 +0200

    feat: port telescope commands to snacks picker

--- config/nvim/lua/custom/plugins/snacks.lua
+++ config/nvim/lua/custom/plugins/snacks.lua
@@ -7,6 +7,50 @@ return {
     explorer = { enabled = true },
   },
   keys = {
+    -- Search
+    {
+      '<leader>sh',
+      function()
+        Snacks.picker.help()
+      end,
+      desc = 'Help Pages',
+    },
+    {
+      '<leader>sk',
+      function()
+        Snacks.picker.keymaps()
+      end,
+      desc = 'Keymaps',
+    },
+    {
+      '<leader>sw',
+      function()
+        Snacks.picker.grep_word()
+      end,
+      desc = 'Visual selection or word',
+      mode = { 'n', 'x' },
+    },
+    {
+      '<leader>sg',
+      function()
+        Snacks.picker.grep()
+      end,
+      desc = 'Grep',
+    },
+    {
+      '<leader>sd',
+      function()
+        Snacks.picker.diagnostics()
+      end,
+      desc = 'Diagnostics',
+    },
+    {
+      '<leader>sD',
+      function()
+        Snacks.picker.diagnostics_buffer()
+      end,
+      desc = 'Buffer Diagnostics',
+    },
     {
       '<leader>sf',
       function()
@@ -14,6 +58,15 @@ return {
       end,
       desc = 'Smart Find Files',
     },
+    {
+      '<leader>sR',
+      function()
+        Snacks.picker.resume()
+      end,
+      desc = 'Resume',
+    },
+
+    -- Explorer
     {
       '<leader>e',
       function()
