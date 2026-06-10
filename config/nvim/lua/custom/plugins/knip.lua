9c8aad53fd338dfd218ac51985ffba9bc1c961ca
Mircea Badragan <mircea.badragan@monsterlynx.com>
2026-06-10 18:58:43 +0300

    chore: disable knip plugin

--- config/nvim/lua/custom/plugins/knip.lua
+++ config/nvim/lua/custom/plugins/knip.lua
@@ -1,4 +1,5 @@
 return {
+  enabled = false,
   dir = vim.fn.stdpath 'config' .. '/pack/github/start/knip.nvim',
   ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   opts = {},
